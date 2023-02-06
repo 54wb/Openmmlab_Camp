3430e9e7748c0bfa7fb3cd368c6e4e4558fcfb03
54wb <1225768798@qq.com>
2023-02-06 16:34:03 +0800

    add advanced hw

---
+++ hw01/advanced/config/efficientnet-b7_b128_cifar10.py
@@ -0,0 +1,20 @@
+_base_ = [
+    '../_base_/models/efficientnet_b7.py',
+    '../_base_/datasets/cifar10_bs16.py',
+    '../_base_/schedules/cifar10_bs128.py',
+    '../_base_/default_runtime.py',
+]
+
+
+model = dict(
+    head=dict(
+        num_classes=10
+    )
+)
+
+data=dict(
+    samples_per_gpu=128,
+    workers_per_gpu=4
+)
+
+load_from = '/disk0/lwb/pretrain/efficientnet-b7_3rdparty_8xb32-aa-advprop_in1k_20220119-c6dbff10.pth'
