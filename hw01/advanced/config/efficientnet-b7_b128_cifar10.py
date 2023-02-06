a2cd67f126d4912e4f5ec36420cd70f676de8f26
54wb <1225768798@qq.com>
2023-02-06 17:32:54 +0800

    fix epochs

--- hw01/advanced/config/efficientnet-b7_b128_cifar10.py
+++ hw01/advanced/config/efficientnet-b7_b128_cifar10.py
@@ -16,5 +16,6 @@ data=dict(
     samples_per_gpu=128,
     workers_per_gpu=4
 )
-
+lr_config = dict(policy='step', step=[70, 100])
+runner = dict(type='EpochBasedRunner', max_epochs=120)
 load_from = '/disk0/lwb/pretrain/efficientnet-b7_3rdparty_8xb32-aa-advprop_in1k_20220119-c6dbff10.pth'
